d8fdd824363ac6a8191bac1e80614b1f3d4337e1
Murilo Spineli <96277238+mainSpi@users.noreply.github.com>
2025-02-08 15:28:04 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("oi")
+import csv
+
+lugares = {
+    "Heitor Beltrão": 8,
+    "João Barros Neto" : 10,
+    "Hesfa/Marcolino" : 12,
+    "Estácio de Sá" : 10,
+    "Felippe Cardoso" : 12
+}
+
+turmas = ["t1.csv", "t2.csv"]
+
+with open("t1.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        print(row)
